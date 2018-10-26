a4be5a0f4338753b16617a71fc11912c3232d600
Jae M. Kim <jaemyung.kim@uwaterloo.ca>
2018-10-26 09:51:40 -0400

    Update run2.sh
    usage message

--- benchmarks/run2.sh
+++ benchmarks/run2.sh
@@ -8,7 +8,7 @@
 
 if [[ $# -lt 4 ]]; then
     echo "Too few arguments. "
-    echo "Usage $0 <executable> <benchmark> <threads> <runtime>"
+    echo "Usage $0 <executable> <benchmark> <threads> <logbuf(MB)>"
     exit
 fi
 
