e583b50d9f14b5e2e5b94de205ab2c0c9a07cc2e
C-lab-1 <shunfany@sfu.ca>
2022-11-08 14:58:03 -0800

    make interface of hash.h and ex_finger.h to accept K-V params

--- dash/Hash.h
+++ dash/Hash.h
@@ -1,38 +1,36 @@
 
-// Copyright (c) Simon Fraser University & The Chinese University of Hong Kong. All rights reserved.
-// Licensed under the MIT license.
+// Copyright (c) Simon Fraser University & The Chinese University of Hong Kong.
+// All rights reserved. Licensed under the MIT license.
 #ifndef HASH_INTERFACE_H_
 #define HASH_INTERFACE_H_
 
 #include "../util/pair.h"
-#ifdef PMEM
-#include <libpmemobj.h>
 #endif
 
 /*
-* Parent function of all hash indexes
-* Used to define the interface of the hash indexes
-*/
+ * Parent function of all hash indexes
+ * Used to define the interface of the hash indexes
+ */
 
-template <class T>
+template <class K, class V>
 class Hash {
  public:
   Hash(void) = default;
   ~Hash(void) = default;
   /*0 means success insert, -1 means this key already exist, directory return*/
-  virtual int Insert(T, Value_t) = 0;
-  virtual int Insert(T, Value_t, bool) = 0;
- 
+  virtual int Insert(K, V) = 0;
+  virtual int Insert(K, V, bool) = 0;
+
   virtual void bootRestore(){
 
   };
   virtual void reportRestore(){
 
   };
-  virtual bool Delete(T) = 0;
-  virtual bool Delete(T, bool) = 0;
-  virtual bool Get(T, Value_t*) = 0;
-  virtual bool Get(T key, Value_t*, bool is_in_epoch) = 0;
+  virtual bool Delete(K) = 0;
+  virtual bool Delete(K, bool) = 0;
+  virtual bool Get(K, V *) = 0;
+  virtual bool Get(K key, V *, bool is_in_epoch) = 0;
   virtual void getNumber() = 0;
 };
 
